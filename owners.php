db5ad7b434c915cee97e10f9dc86f1b759bf1c36
Kashif Khan <kashifkhan@Kashifs-MacBook-Pro.local>
2015-12-01 04:09:04 -0800

    kk

---
+++ owners.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" id="facebook" class="no_js">
+<head><meta charset="utf-8" />
+</head>
+<style>
+.t0 {
+
+} 
+
+.t0 tr:nth-child(odd){ background-color:rgba(150,200,250,0.8); }
+.t0 tr:nth-child(even) { background-color:rgba(200,225,250,0.8); }
+.t0 td{ vertical-align:middle;padding:8px;} 
+</style>
+<body>
+<h4>BA</h4>
+ 
+<table class="t0">
+ <tr><td>importdcim3.py</td><td>import dscs from sdcards </td> </tr>
+<tr><td>scanDSC.py </td><td>scan existing DSCs </td> </tr>
+<tr><td>dncve.py </td><td>akaze detect and compute for tiles</td> </tr>
+<tr><td>dncDSC.py </td><td>akazedetect and compute for DSCs</td> </tr>
+ </table>
+
+
+<h4>KK</h4>
+ 
+<table class="t0">
+<tr><td>homog.py</td><td>homography </td> </tr>
+<tr><td>mastermongo.py</td><td>gen</td> </tr>
+ 
+ </table>
+
+
+<h4>OB</h4>
+ 
+<table class="t0">
+<tr><td>docker.py</td><td>  </td> </tr>
+<tr><td>cootainer.py</td><td> </td> </tr>
+ 
+ </table>
+
+</body>
